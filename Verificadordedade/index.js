c0d9a85dfc8ee71cb0ef417595a91d5b2d495539
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-06 20:30:16 -0300

    update

---
+++ Verificadordedade/index.js
@@ -0,0 +1,16 @@
+function verificar(){
+    let nascimento = document.querySelector('.anoDeNascimento')
+    let idade = Number(nascimento.value)
+    let res = document.querySelector('.res')
+    res.innerHTML = `você tem ${idade} anos de idade`
+
+
+
+
+
+
+
+
+
+
+}
