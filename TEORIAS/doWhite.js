884a19a4d197f8a31ab7b2d6f7204e8f079bfe76
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-01-09 13:08:59 -0300

    update

---
+++ TEORIAS/doWhite.js
@@ -0,0 +1,27 @@
+//a repetição vai acontecer enquanto for verdade, quando for false ele para. 
+//ex com uma pizza
+
+
+
+//looping até acabar a pizza, quando não tiver fatia ele vira false e acaba o looping.
+
+/*function comerPizza(){
+    while(temfatia()){
+        comerFatia()
+    }
+}
+
+
+
+*/
+let c = 1
+while(c < 6){
+    console.log('tudo bem')
+    c++
+}
+
+
+let x = 10
+for(a = 0; a < x; a++){
+    console.log(a+1)
+}
