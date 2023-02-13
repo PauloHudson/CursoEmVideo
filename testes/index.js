f7ed5161217366c3fcc2ec32f63b7fd515c9c903
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-02-13 13:36:21 -0300

    up

---
+++ testes/index.js
@@ -0,0 +1,8 @@
+
+function mudarcor(){
+    let fundo = document.querySelector('.container')
+    let corReceber = document.querySelector('.cor').value
+    fundo.style.backgroundColor = corReceber
+
+
+}
