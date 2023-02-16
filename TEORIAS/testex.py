c4979e8fc2a05a0aa8f07a4ba4d4c0857ada3cdb
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2023-02-16 15:52:37 -0300

    sup

---
+++ TEORIAS/testex.py
@@ -0,0 +1,27 @@
+# salario = int(input('digite seu salário: '))
+# idade = int(input('digite sua idade: '))
+
+# if(salario > 40000 and idade > 18 ):
+#     print('aprovado')
+# else:
+#     print("reprovado")
+
+# teAmo = "EuteAmo"
+# print(teAmo[2])
+
+# array = ["oi", "iu", "te amo"]
+
+# for elemento in array:
+#     for i in range(10):
+#         print(elemento)
+
+nome = 'tiao'
+idade = 20
+print('tiao tem %d anos' % idade )
+print(' %s tem 20 anos de idade' % nome)
+
+numero = 20
+print("[%01d]" % numero)
+
+dinheiro = 52.34
+print('%s tem %d de idade e [R$%5.1f]' % (nome, idade, dinheiro))
